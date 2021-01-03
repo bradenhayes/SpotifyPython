451c98e26a7570eeba7db0cb389f985271d8be6a
Braden Hayes <bradenhayes@cmail.carleton.ca>
2021-01-03 13:31:11 -0500

    Added playback when device is available

--- PlaySpotify.py
+++ PlaySpotify.py
@@ -12,7 +12,7 @@ username = "bradenhayes55"
 redirect_url='http://localhost:8000'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-scope = "playlist-modify-public playlist-modify-private playlist-read-private"
+scope = "playlist-modify-public playlist-modify-private playlist-read-private user-modify-playback-state"
 token = util.prompt_for_user_token(username, scope,cid,secret,redirect_url)
 
 
@@ -28,15 +28,15 @@ def create_and_add(ids):
     try:
         for x in playlist_json['items']:
             playlist_id.insert(0,str(x['id']))
-            #print(playlist_id[0])
                 
     except:
             print("Failed to parse JSON")
     
     sp.user_playlist_add_tracks(username,playlist_id[0],ids)
-    
-    
-
+    try:
+        sp.start_playback(device_id='15f2001582a255b113c082f8327e1dcbc21e4ea8',context_uri="spotify:playlist:" +playlist_id[0])
+    except:
+            print("Device is offline")
 
     
 def getTrackIDs(user, playlist_id):
@@ -50,10 +50,9 @@ def getTrackIDs(user, playlist_id):
 
 def new_playlist():
     ids = getTrackIDs(username, '37i9dQZEVXbwTsxvuo6IFR')
-    #print(ids)
     create_and_add(ids)
     
-schedule.every().friday.at('16:30').do(new_playlist)
+schedule.every().sunday.at('13:24').do(new_playlist)
 while 1:
     schedule.run_pending()
     time.sleep(1)
